bf34bda21d7ed67abab571469563fb3172b074d6
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2018-08-12 19:47:17 -0300

    Config eslint and install new modules

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,20 @@
-import { configure } from 'enzyme'
+import React from 'react'
+import {
+  configure,
+  shallow,
+  render,
+  mount,
+} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { createSerializer } from 'enzyme-to-json'
+import sinon from 'sinon'
+
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }))
 
 configure({ adapter: new Adapter() })
+
+global.React = React
+global.shallow = shallow
+global.render = render
+global.mount = mount
+global.sinon = sinon
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,16 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
 
-import reducer from '../reducers/'
+import reducer from '../reducers'
 
-const configureStore = (preloadedState) => createStore(reducer, preloadedState, applyMiddleware(thunkMiddleware));
+/* eslint no-underscore-dangle: 0 */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
+const configureStore = preloadedState => createStore(
+  reducer,
+  preloadedState,
+  composeEnhancers(
+    applyMiddleware(thunk),
+  ),
+)
 export default configureStore
